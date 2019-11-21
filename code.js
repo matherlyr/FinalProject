9f82d35324b40e460be21217bcab26f86b991001
Rachel K. Matherly <rachel.matherly@centre.edu>
2019-11-21 15:39:31 -0500

    start

--- code.js
+++ code.js
@@ -1,1 +1,30 @@
+var Promise = d3.csv("EduFilms.csv")
+var success = function(data)
+    {
+        console.log(data)
+        console.log(data.map(getfilm))
+        
+    }
+var fail = function(data)
+    {
+        console.log("fail")
+    }
+Promise.then(success,fail)
+
+var PPromise = d3.csv("Participants.csv")
+var success = function(data)
+    {
+        console.log(data)
+    }
+var fail = function(data)
+    {
+        console.log("fail")
+    }
+PPromise.then(success,fail)
+
+
+var  getfilm = function(d)
+{
+    return d.FilmOnce;
+}
 
