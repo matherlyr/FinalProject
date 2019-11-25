fcaf7899e167e9a3632373856e9975fc6c308972
Rachel Matherly <rachel.matherly@centre.edu>
2019-11-25 09:57:39 -0500

    help

--- code.js
+++ code.js
@@ -2,8 +2,9 @@ var Promise = d3.csv("EduFilms.csv")
 var success = function(data)
     {
         console.log(data)
-        console.log(FreeRecall8)
-        console.log(data.map(getfilm))
+        //console.log(FreeRecall8)
+        //console.log(data.map(getfilm))
+        //setup(data)
         
     }
 var fail = function(data)
@@ -24,39 +25,45 @@ var fail = function(data)
 PPromise.then(success,fail)
 
 
-var  getfilm = function(d)
-{
-    return d.FilmOnce;
-}
-
-var FreeRecall8 = [data[0].SchoolLessonandFilm , data[0].FilmRepetition]
-FreeRecall8[0]=data[0].SchoolLessonandFilm
-FreeRecall8[1]=data[0].FilmRepetition
+//var  getfilm = function(d)
+//{
+//    return d.FilmOnce;
+//}
+//
+//var FreeRecall8 = [data[0].SchoolLessonandFilm , data[0].FilmRepetition]
+//FreeRecall8[0]=data[0].SchoolLessonandFilm
+//FreeRecall8[1]=data[0].FilmRepetition
 
 var margin = {top: 80, right: 80, bottom: 80, left: 80},
-    width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+ width = 600 - margin.left - margin.right,
+ height = 400 - margin.top - margin.bottom;
+
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
-var y0 = d3.scale.linear().domain([300, 1100]).range([height, 0]),
-y1 = d3.scale.linear().domain([20, 80]).range([height, 0]);
-var xAxis = d3.svg.axis()
+
+var setup = function(data)
+{
+    var y0 = d3.scale.linear().domain([300, 1100]).range([height, 0]),
+        y1 = d3.scale.linear().domain([20, 80]).range([height, 0]);
+    var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 // create left yAxis
-var yAxisLeft = d3.svg.axis().scale(y0).ticks(4).orient("left");
+    var yAxisLeft = d3.svg.axis().scale(y0).ticks(4).orient("left");
 // create right yAxis
-var yAxisRight = d3.svg.axis().scale(y1).ticks(6).orient("right");
-var svg = d3.select("body").append("svg")
+    var yAxisRight = d3.svg.axis().scale(y1).ticks(6).orient("right");
+    var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("class", "graph")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-d3.tsv("data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.year; }));
-  y0.domain([0, d3.max(data, function(d) { return d.money; })]);
-  
+    
+    createBars(data);
+}
+
+var createBars=function(data)
+{
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -70,7 +77,7 @@ d3.tsv("data.tsv", type, function(error, data) {
 	  .attr("dy", "-2em")
 	  .style("text-anchor", "end")
 	  .style("text-anchor", "end")
-	  .text("Dollars");
+	  .text("Average Percent Correct");
 	
   svg.append("g")
 	  .attr("class", "y axis axisRight")
@@ -82,17 +89,18 @@ d3.tsv("data.tsv", type, function(error, data) {
 	  .attr("dx", "2em")
 	  .style("text-anchor", "end")
 	  .text("#");
-  bars = svg.selectAll(".bar").data(data).enter();
-  bars.append("rect")
+  var bars = svg.selectAll(".bar").data(data).enter();
+    bars.append("rect")
       .attr("class", "bar1")
-      .attr("x", function(d) { return x(d.year); })
+      .attr("x", function(d) { return x(d.8yearsold); })
       .attr("width", x.rangeBand()/2)
-      .attr("y", function(d) { return y0(d.money); })
-	  .attr("height", function(d,i,j) { return height - y0(d.money); }); 
-  bars.append("rect")
+      .attr("y", function(d) { return y0(d.SchoolLessonandFilm); })
+	  .attr("height", function(d,i,j) { return height - y0(d.SchoolLessonandFilm); }); 
+    bars.append("rect")
       .attr("class", "bar2")
-      .attr("x", function(d) { return x(d.year) + x.rangeBand()/2; })
+      .attr("x", function(d) { return x(d.10yearold) + x.rangeBand()/2; })
       .attr("width", x.rangeBand() / 2)
-      .attr("y", function(d) { return y1(d.number); })
-	  .attr("height", function(d,i,j) { return height - y1(d.number); }); 
+      .attr("y", function(d) { return y1(d.SchoolLessonandFilm); })
+	  .attr("height", function(d,i,j) { return height - y1(d.SchoolLessonandFilm); }); 
 
+    }
